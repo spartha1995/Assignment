a249020edc8e354c3403d01234adef2196ce266f
Partha Sarathi <partha@promact.com>
2017-02-14 18:49:33 +0530

    ALL ASSIGMENT ADDED

---
+++ csharp/BangaloreBank/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BangaloreBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] digits;
+
+            Console.WriteLine("Enter a bank account number: ");
+            string bankAccountNumber = Console.ReadLine();
+
+            digits = new int[bankAccountNumber.Length];
+
+            //String to integer array
+            for (int i = 0; i < bankAccountNumber.Length; i++)
+            {
+                digits[i] = int.Parse(bankAccountNumber.ElementAt(i)+"");
+                if(digits[i] == 0)
+                {
+                    digits[i] = 10;
+                }
+            }
+
+            int leftIndex = digits[1];
+            int rightIndex = digits[0];
+
+           
+
+            int currentDigit, timeTaken = 0, closeKey;
+
+            for(int i = 0; i < digits.Length; i++)
+            {
+                currentDigit = digits[i];
+
+                closeKey = FindCloseKey(rightIndex, currentDigit, leftIndex);
+
+                timeTaken += Math.Abs(closeKey - currentDigit) + 1; //+1 second for key press
+
+                if (closeKey == rightIndex)
+                    rightIndex = currentDigit;
+                else
+                    leftIndex = currentDigit;
+
+            }
+
+            Console.WriteLine("It would take " + timeTaken + " seconds to type the bank account");
+
+
+            Console.Read();
+            
+
+        }
+
+        private static int FindCloseKey(int rightIndex, int currentDigit, int leftIndex)
+        {
+            return (Math.Abs(currentDigit - rightIndex) < Math.Abs(leftIndex - currentDigit)) ? rightIndex : leftIndex;
+        }
+    }
+}
---
+++ csharp/InheritanceFunction/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritanceFunction
+{
+    class A
+    {
+        protected void MultiplyBy2(ref int a)
+        {
+            a = a * 2;
+        }
+    }
+
+    class B : A
+    {
+        protected void MultiplyBy3(ref int a)
+        {
+            a = a * 3;
+        }
+    }
+
+    class C : B
+    {
+        protected void MultiplyBy5(ref int a)
+        {
+            a = a * 5;
+        }
+    }
+    
+    class D : C
+    {
+        static void Main(string[] args)
+        {
+            int initialValue = 1;
+            int callACount = 0;
+            int callBCount = 0;
+            int callCCount = 0;
+            bool numberMatched = false;
+
+            D obj = new D();
+            
+            Console.WriteLine("Enter a final value (It should be a factor of 2, 3 & 5):");
+            int finalValue = int.Parse(Console.ReadLine());
+            int tempValue = finalValue;
+
+            while (!numberMatched)
+            {
+                if(tempValue % 2 == 0)
+                {
+                    obj.MultiplyBy2(ref initialValue);
+                    tempValue /= 2;
+                    callACount++;
+                }else if(tempValue % 3 == 0)
+                {
+                    obj.MultiplyBy3(ref initialValue);
+                    tempValue /= 3;
+                    callBCount++;
+                }else if(tempValue % 5 == 0)
+                {
+                    obj.MultiplyBy5(ref initialValue);
+                    tempValue /= 5;
+                    callCCount++;
+                }else
+                {
+                    break;
+                }
+
+                if (initialValue == finalValue)
+                {
+                    numberMatched = true;
+                }
+            }
+
+            if (numberMatched)
+            {
+                Console.WriteLine("A's function is called: " + callACount);
+                Console.WriteLine("B's function is called: " + callBCount);
+                Console.WriteLine("C's function is called: " + callCCount);
+            }else
+            {
+                Console.WriteLine("The number given is not a factor of 2, 3, 5");
+            }
+
+            Console.Read();
+        }
+    }
+}
